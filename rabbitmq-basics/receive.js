250e775fa5730b9db69c3fb22f319ae41fa9487d
Sanket Pawar <sanketpawarsp7781@gmail.com>
2026-06-10 19:36:13 +0530

    helloworld:rabbitmq

---
+++ rabbitmq-basics/receive.js
@@ -0,0 +1,27 @@
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const queue = "hello";
+
+  await channel.assertQueue(queue, {
+    durable: true,
+    arguments: { "x-queue-type": "quorum" },
+  });
+
+  console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+  channel.consume(
+    queue,
+    function (msg) {
+      console.log(" [x] Received %s", msg.content.toString());
+    },
+    {
+      noAck: true,
+    },
+  );
+}
+
+main();
---
+++ rabbitmq-basics/send.js
@@ -0,0 +1,24 @@
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const queue = "hello";
+  const msg = "Hello World!";
+
+  await channel.assertQueue(queue, {
+    durable: true,
+    arguments: { "x-queue-type": "quorum" },
+  });
+  channel.sendToQueue(queue, Buffer.from(msg));
+
+  console.log(" [x] Sent %s", msg);
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+}
+
+main();
