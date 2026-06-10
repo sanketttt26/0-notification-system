a19e6c24a2510d406b9e6eb4060e9169bf23e7f5
Sanket Pawar <sanketpawarsp7781@gmail.com>
2026-06-10 19:50:55 +0530

    task queues

---
+++ rabbitmq-basics/new_task.js
@@ -0,0 +1,27 @@
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const queue = "task_queue";
+  const msg = process.argv.slice(2).join(" ") || "Hello World!";
+
+  await channel.assertQueue(queue, {
+    durable: true,
+    arguments: {
+      "x-queue-type": "quorum",
+    },
+  });
+  channel.sendToQueue(queue, Buffer.from(msg), {
+    persistent: true,
+  });
+  console.log(" [x] Sent '%s'", msg);
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+}
+
+main();
---
+++ rabbitmq-basics/worker.js
@@ -0,0 +1,34 @@
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const queue = "task_queue";
+
+  await channel.assertQueue(queue, {
+    durable: true,
+    arguments: {
+      "x-queue-type": "quorum",
+    },
+  });
+  channel.prefetch(1);
+  console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+  channel.consume(
+    queue,
+    function (msg) {
+      const secs = msg.content.toString().split(".").length - 1;
+
+      console.log(" [x] Received %s", msg.content.toString());
+      setTimeout(function () {
+        console.log(" [x] Done");
+        channel.ack(msg);
+      }, secs * 1000);
+    },
+    {
+      noAck: false,
+    },
+  );
+}
+
+main();
