8176d5c6ca2fafc8dd428f1301dda272605d321f
Sanket Pawar <sanketpawarsp7781@gmail.com>
2026-06-14 22:02:20 +0530

    direct exchange

---
+++ rabbitmq-basics/emit_log_direct.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchange = "direct_logs";
+  const args = process.argv.slice(2);
+  const msg = args.slice(1).join(" ") || "Hello World!";
+  const severity = args.length > 0 ? args[0] : "info";
+
+  await channel.assertExchange(exchange, "direct", {
+    durable: false,
+  });
+  channel.publish(exchange, severity, Buffer.from(msg));
+  console.log(" [x] Sent %s: '%s'", severity, msg);
+
+  setTimeout(function () {
+    connection.close();
+
+    process.exit(0);
+  }, 500);
+}
+
+main();
---
+++ rabbitmq-basics/receive_log_direct.js
@@ -0,0 +1,44 @@
+const amqp = require("amqplib");
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log("Usage: receive_log_direct.js [info] [warning] [error]");
+  process.exit(1);
+}
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchange = "direct_logs";
+
+  await channel.assertExchange(exchange, "direct", {
+    durable: false,
+  });
+
+  const q = await channel.assertQueue("", {
+    exclusive: true,
+  });
+  console.log(" [*] Waiting for logs. To exit press CTRL+C");
+
+  for (const severity of args) {
+    await channel.bindQueue(q.queue, exchange, severity);
+  }
+
+  channel.consume(
+    q.queue,
+    function (msg) {
+      console.log(
+        " [x] %s: '%s'",
+        msg.fields.routingKey,
+        msg.content.toString(),
+      );
+    },
+    {
+      noAck: true,
+    },
+  );
+}
+
+main();
