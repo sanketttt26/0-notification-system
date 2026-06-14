67e8ecf32f243be2b654251ab8302a3121af93ca
Sanket Pawar <sanketpawarsp7781@gmail.com>
2026-06-14 21:44:34 +0530

    multiple consumers

---
+++ rabbitmq-basics/emit_logs.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchange = "logs";
+  const msg = process.argv.slice(2).join(" ") || "Hello World!";
+
+  await channel.assertExchange(exchange, "fanout", {
+    durable: false,
+  });
+  channel.publish(exchange, "", Buffer.from(msg));
+  console.log(" [x] Sent %s", msg);
+
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
+}
+
+main();
---
+++ rabbitmq-basics/receive_logs.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const amqp = require("amqplib");
+
+async function main() {
+  const connection = await amqp.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const exchange = "logs";
+
+  await channel.assertExchange(exchange, "fanout", {
+    durable: false,
+  });
+
+  const q = await channel.assertQueue("", {
+    exclusive: true,
+  });
+  console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+  channel.bindQueue(q.queue, exchange, "");
+
+  channel.consume(
+    q.queue,
+    function (msg) {
+      if (msg.content) {
+        console.log(" [x] %s", msg.content.toString());
+      }
+    },
+    {
+      noAck: true,
+    },
+  );
+}
+
+main();
